e65ff9ec681105f810624506f9bbbeb37ed33882
togrulisayev <tisazade96@gmail.com>
2024-11-21 14:41:02 +0400

    İlk commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// let pet = "golden retriever";
+// let result = '"' + pet.toUpperCase() + '"'; // burda stringleri birleshdirdim
+// console.log(result); // ve netice : "GOLDEN RETRIEVER"
+
+// let question = "Can you solve this problem";
+// console.log(question.length); // netice:26
+
+// let activity = "This game is boring";
+// let result = activity.replace("boring", "exciting");
+// console.log(result); // netice : "This game is exciting"
+
+// let headline = "Choose Your Adventure!";
+// let word = headline.slice(12, 21); // "Adventure" basladigi index 12, son index 21
+// console.log(word);
+
+// let desserts = ["Ice Cream", "Pie", "Pudding"];
+// desserts.push("Cake");
+// console.log(desserts); // netice: ["Ice Cream", "Pie", "Pudding", "Cake"]
+
+// let cities = ["London", "New York", "Tokyo"];
+// let Paris = cities.includes("Paris");
+// console.log(Paris); // netice: false
+
+
+// let cars = ["Sedan", "Coupe"];
+// let trucks = ["Pickup", "SUV"];
+// let connect = cars.concat(trucks);
+// console.log(connect); // netice: ["Sedan", "Coupe", "Pickup", "SUV"]
+
+
+// let desserts = ["Ice Cream", "Pie", "Pudding", "Cake"];
+// let find = desserts.indexOf("Pie");
+// console.log(find); // netice 1
+
+// let word = "Technology";
+// let reversword = word.split("").reverse().join(""); // split metodu arraye cevirdim,reversele tersine cevirdim, joinle birleshdirdim
+// console.log(reversdWord); // netice: "ygolonhceT"
